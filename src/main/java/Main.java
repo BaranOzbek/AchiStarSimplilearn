07460b9db55687042eae0381151991563bfa76aa
Baran Ozbek <xitenebula@gmail.com>
2022-02-03 16:48:44 +0000

    Add main class file

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,10 @@
-package PACKAGE_NAME;
-
 /**
  * Created by Baran on 03/02/2022.
  */
 public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello there from far away my friend.");
+
+    }
 }
